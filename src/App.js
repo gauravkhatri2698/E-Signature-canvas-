26c844f2119c44f3d44db566fd2a6ebeb463f5d0
Gaurav Khatri <gaurav.khatri@polestarllp.com>
2022-03-08 17:51:14 +0530

    Added e-signature using react-signature-canvas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import SignaturePad from 'react-signature-canvas';
+import './canvas.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [sign_image, setImage] = useState(null);
+
+  const canvas = useRef({});
+
+  const clear = () => canvas.current.clear();
+
+  const save = () => setImage(canvas.current.getTrimmedCanvas().toDataURL('image/png'));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Signature Pad Example</h1>
+      <Popup modal trigger={<button>Open Signature Pad</button>} closeOnDocumentClick={false}>
+        {
+          (close) => (
+            <>
+              <SignaturePad
+                ref={canvas}
+                canvasProps={{
+                  className: "signatureCanvas"
+                }}
+                clearOnResize={false}
+              // backgroundColor='black'
+              // penColor='white'
+              />
+              <button className='save_canvas' onClick={save}>Save</button>
+              <button className='clear_canvas' onClick={clear}>Clear</button>
+              <button className='close_canvas' onClick={close}>Close</button>
+            </>
+          )
+        }
+      </Popup>
+
+      <br />
+      <br />
+
+      {
+        (sign_image ? (
+          <img
+            src={sign_image}
+            alt="my signature"
+            style={{
+              display: 'block',
+              margin: '0 auto',
+              border: '1px solid black',
+              width: '150px'
+            }}
+          >
+          </img>
+        ) : null)
+      }
     </div>
   );
 }
